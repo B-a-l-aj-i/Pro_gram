df585e9aee90ecbea3e88e5e717979060966a2c0
B-a-l-aj-i <balajivijay679@gmail.com>
2025-05-17 19:16:10 +0530

    Refactor middleware functions for the "/test" route and remove commented-out code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 import express from "express";
 
-
 const app = express();
 
+// app.get("/user/:id", (req, res) => {
+//   console.log(req);
+//   res.send(req.params);
+// });
 
+// app.use("/test", (req, res) => {
+//   res.send("test successfull using app.use()");
+// });
 
-app.get("/user/:id", (req, res) => {
+app.use(
+  "/test",
+  (req, res, next) => {
+    console.log("Middleware function executed");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Second middleware function executed");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Third middleware function executed");
+    res.send("test 3 successfull ");
+  }
+);
 
-    res.send(req.params)
-})
-
-
-app.use("/test", (req, res) => {
-    res.send("test successfull using app.use()");
-})
-
-    
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
