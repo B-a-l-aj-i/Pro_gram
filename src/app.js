5fff0920f3a19b645e2bd84f88af736175552fd9
B-a-l-aj-i <balajivijay679@gmail.com>
2025-05-17 21:07:52 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -2,30 +2,21 @@ import express from "express";
 
 const app = express();
 
-// app.get("/user/:id", (req, res) => {
-//   console.log(req);
-//   res.send(req.params);
+// app.use("/", (req, res, next) => {
+//   console.log("Middleware for all routes");
+//   res.send("Hello from the root route");
+//   next();
 // });
 
-// app.use("/test", (req, res) => {
-//   res.send("test successfull using app.use()");
-// });
+app.get("/test", (req, res, next) => {
+  console.log("Middleware for /test route");
+  // res.send("Hello from the /test  1 route");
+  next();
+});
 
-app.use(
-  "/test",
-  (req, res, next) => {
-    console.log("Middleware function executed");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Second middleware function executed");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Third middleware function executed");
-    res.send("test 3 successfull ");
-  }
-);
+app.get("/test", (req, res) => {
+  res.send("Hello from /test route");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
